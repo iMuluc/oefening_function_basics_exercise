eae3602ea4e44f5ddd86ced0b7d655ceea292da4
iMuluc <iMertUluc@gmail.com>
2021-08-12 17:25:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const makePizza = function () {
+    console.log("Get the dough");
+    console.log("Get the sauce");
+    console.log("Get the toppings");
+}
+/*
+makePizza();
+makePizza();
+makePizza();
+
+console.log("")
+*/
+const makeSushi = function () {
+    console.log("Get the rice");
+    console.log("Get the fish");
+    console.log("Get the sauce");
+}
+/*
+makeSushi();
+
+console.log("");
+*/
+
+makeSushi();
+makeSushi();
+makeSushi();
+console.log("");
+makePizza();
+makePizza();
